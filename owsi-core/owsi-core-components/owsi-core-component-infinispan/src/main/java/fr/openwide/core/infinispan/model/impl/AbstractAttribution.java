da3fb1bf45f196413d8be348d04a14a630291d06
Laurent Almeras <laurent.almeras@openwide.fr>
2017-04-27 10:24:55 +0200

    switch to org.groups.Address so that beans are serializable in wicket

--- owsi-core/owsi-core-components/owsi-core-component-infinispan/src/main/java/fr/openwide/core/infinispan/model/impl/AbstractAttribution.java
+++ owsi-core/owsi-core-components/owsi-core-component-infinispan/src/main/java/fr/openwide/core/infinispan/model/impl/AbstractAttribution.java
@@ -2,7 +2,7 @@ package fr.openwide.core.infinispan.model.impl;
 
 import java.util.Date;
 
-import org.infinispan.remoting.transport.Address;
+import org.jgroups.Address;
 
 import com.google.common.base.Objects;
 
