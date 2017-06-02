rjoly/owsi-core-parent

add flywaydb dependencies and properties
add flywaydb bean creation

add some examples of flyway Java class migrations

owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/config/util/FlywayConfiguration.java
@@ -1,0 +1,28 @@
+package fr.openwide.core.jpa.more.config.util;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class FlywayConfiguration {
+
+	@Value("${flyway.locations}")
+	private String locations;
+	
+	@Value("${flyway.schemas}")
+	private String schemas;
+	
+	@Value("${flyway.table}")
+	private String table;
+
+	public String getLocations() {
+		return locations;
+	}
+
+	public String getSchemas() {
+		return schemas;
+	}
+
+	public String getTable() {
+		return table;
+	}
+	
+}

xg427/igloo-parent

basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/config/util/FlywayConfiguration.java
@@ -1,0 +1,28 @@
+package fr.openwide.core.basicapp.core.config.util;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class FlywayConfiguration {
+
+	@Value("${flyway.locations}")
+	private String locations;
+	
+	@Value("${flyway.schemas}")
+	private String schemas;
+	
+	@Value("${flyway.table}")
+	private String table;
+
+	public String getLocations() {
+		return locations;
+	}
+
+	public String getSchemas() {
+		return schemas;
+	}
+
+	public String getTable() {
+		return table;
+	}
+	
+}