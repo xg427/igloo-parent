5e5f3ed6b750a6adf62e83128911366d7ec9951f
Laurent Almeras <laurent.almeras@openwide.fr>
2017-06-07 18:09:23 +0200

    switching to hibernate-search 5.8.0.Beta2

--- owsi-core/owsi-core-components/owsi-core-component-elasticsearch-plugin/src/main/java/index/CoreFrenchMinimalStemFilterFactory.java
+++ owsi-core/owsi-core-components/owsi-core-component-elasticsearch-plugin/src/main/java/index/CoreFrenchMinimalStemFilterFactory.java
@@ -4,18 +4,17 @@ import org.apache.lucene.analysis.TokenStream;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.env.Environment;
-import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
-import org.elasticsearch.index.settings.IndexSettingsService;
 
 public class CoreFrenchMinimalStemFilterFactory extends AbstractTokenFilterFactory {
 
 	@Inject
-	public CoreFrenchMinimalStemFilterFactory(Index index, IndexSettingsService indexSettingsService, Environment env, @Assisted String name, @Assisted Settings settings) {
-		super(index, indexSettingsService.getSettings(), name, settings);
+	public CoreFrenchMinimalStemFilterFactory(IndexSettings indexSettings, @Assisted String name, @Assisted Settings settings) {
+		super(indexSettings, name, settings);
 	}
 
+	@Override
 	public TokenStream create(TokenStream input) {
 		return new CoreFrenchMinimalStemFilter(input);
 	}
--- owsi-core/owsi-core-components/owsi-core-component-elasticsearch-plugin/src/main/java/plugin/CoreFrenchMinimalStemPlugin.java
+++ owsi-core/owsi-core-components/owsi-core-component-elasticsearch-plugin/src/main/java/plugin/CoreFrenchMinimalStemPlugin.java
@@ -1,8 +1,13 @@
 package plugin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.analysis.AnalysisModule;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
+import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.AnalysisPlugin;
 import org.elasticsearch.plugins.Plugin;
 
 import index.CoreFrenchMinimalStemFilterFactory;
@@ -10,26 +15,20 @@ import index.CoreFrenchMinimalStemFilterFactory;
 /**
  * Example of a plugin.
  */
-public class CoreFrenchMinimalStemPlugin extends Plugin {
+public class CoreFrenchMinimalStemPlugin extends Plugin implements ActionPlugin, AnalysisPlugin {
 
-    private final Settings settings;
+	private final Settings settings;
 
-    public CoreFrenchMinimalStemPlugin(Settings settings) {
-        this.settings = settings;
-    }
+	public CoreFrenchMinimalStemPlugin(Settings settings) {
+		super();
+		this.settings = settings;
+	}
 
-    @Override
-    public String name() {
-        return "jvm-example";
-    }
-
-    @Override
-    public String description() {
-        return "A plugin that extends all extension points";
-    }
-
-    public void onModule(AnalysisModule module) {
-    	module.addTokenFilter("corefrenchminimalstem", (Class<? extends TokenFilterFactory>) CoreFrenchMinimalStemFilterFactory.class);
-    }
+	@Override
+	public Map<String, AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
+		final Map<String, AnalysisProvider<TokenFilterFactory>> extra = new HashMap<>();
+		extra.put("corefrenchminimalstem", (indexSettings, env, name, settings) -> new CoreFrenchMinimalStemFilterFactory(indexSettings, name, settings));
+		return extra;
+	}
 
 }
